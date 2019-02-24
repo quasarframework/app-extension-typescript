1710fae2b532b227a2986751b320e5855dab481b
Noah Klayman <noahklayman@gmail.com>
2019-02-24 14:06:17 -0800

    feat(webpack): type-check Vue SFCs

--- typescript/index.js
+++ typescript/index.js
@@ -5,23 +5,20 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-module.exports = function (api, ctx) {
+module.exports = function(api, ctx) {
   api.chainWebpack((chain, invoke) => {
-    const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-    chain.resolve
-      .extensions
-        .add('.ts')
-        .add('.tsx')
+    const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+    chain.resolve.extensions.add('.ts').add('.tsx')
     chain.module
       .rule('typescript')
-        .test(/\.tsx?$/)
-        .use('typescript')
-          .loader('ts-loader')
-          .options({
-            appendTsSuffixTo: [/\.vue$/],
-            // Type checking is handled by fork-ts-checker-webpack-plugin
-            transpileOnly: true
-          })
-    chain.plugin('ts-checker').use(ForkTsCheckerWebpackPlugin)
+      .test(/\.tsx?$/)
+      .use('typescript')
+      .loader('ts-loader')
+      .options({
+        appendTsSuffixTo: [/\.vue$/],
+        // Type checking is handled by fork-ts-checker-webpack-plugin
+        transpileOnly: true
+      })
+    chain.plugin('ts-checker').use(ForkTsCheckerWebpackPlugin, [{ vue: true }])
   })
 }
