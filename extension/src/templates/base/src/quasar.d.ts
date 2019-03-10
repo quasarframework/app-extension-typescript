59eb8465930197d0cb5aae868054654677cbebce
Kerry Huguet <>
2019-03-09 21:10:51 -0600

    Remove error in quasar.d.ts
    Remove error in quasar.d.ts
    Remove semicolons

--- extension/src/templates/base/src/quasar.d.ts
+++ extension/src/templates/base/src/quasar.d.ts
@@ -1,16 +1,1 @@
-import Vue from 'vue';
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $q: any;
-  }
-}
-
-// ComponentOptions is declared in types/options.d.ts
-declare module 'vue/types/options' {
-  interface ComponentOptions<V extends Vue> {
-    preFectch?: (options: any) => void | Promise<void>;
-  }
-}
-
-declare module 'quasar';
+declare module 'quasar'
